22037fc02acbbc9af16a1e73d44563b3375c438f
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2026-07-10 20:48:39 -0400

    - in `/src/funcdefs.ts`, fix a type issue with `timer_id` that occurs in more recent versions of deno (`>=v2.8`), thanks to its _bETteR NoDE cOMpAtIBiLiTy_. - in `/deno.json`, modify the `package.json` based `scripts` to make node-based imports set to external. - under `/.github/actions/` and `/.github/workflows/`, update gh-action dependencies:   - `actions/checkout`: `v4` -> `v7`.   - `actions/setup-node`: `v3` -> `v6`.   - `node-version`: `22` -> `24`.

--- src/funcdefs.ts
+++ src/funcdefs.ts
@@ -36,7 +36,9 @@ export const cancelableDelayedPromiseResolver = <T>(
 	logger: LoggerFunction = noopLogger,
 ): [resolve: (value: T) => void, cancel: (delay?: number) => void] => {
 	let
-		timer_id = -1,
+		// bloody deno's node-compatibility "enhancements" is creating more incompatibilities with existing well known web-compatible constructs.
+		// man, do I hate the direction deno is heading. this didn't use to be an issue pre-v2.8 of deno.
+		timer_id: number | ReturnType<typeof setTimeout> = -1,
 		delay = original_delay
 	const
 		resolve = (value: T) => {
